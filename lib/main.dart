7001aac42c8332d52f9e871cfe93f236079b53af
Ari Bambang Kurniawan <aribambang@Aris-MacBook-Pro.local>
2019-06-04 21:21:58 +0700

    add product list

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/rendering.dart';
 
-main() {
+import './product_manager.dart';
+
+void main() {
+  // debugPaintSizeEnabled = true;
+  // debugPaintBaselinesEnabled = true;
+  // debugPaintPointersEnabled = true;
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  build(context) {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
+      // debugShowMaterialGrid: true,
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primarySwatch: Colors.deepOrange,
+          accentColor: Colors.deepPurple),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Fluffy'),
+          title: Text('EasyList'),
         ),
+        body: ProductManager(),
       ),
     );
   }
